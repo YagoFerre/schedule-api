6bf74ddba39eb864b3e0e4de94d3d6d9336fa536
yagof <yagofer12@gmail.com>
2025-04-11 23:55:29 -0300

    next steps

---
+++ adapters/in/services/service.go
@@ -0,0 +1,6 @@
+package services
+
+// mudar nome do arquivo
+// criado para usar mappers aqui ao invés de usar mappers no controller
+// agendamento_service.go já é a implementação de usecases
+// devemos injetar usecases nesse arquivo
