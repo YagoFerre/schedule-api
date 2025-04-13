19c8e1653c0c05a2971a9383c1f20bd6ab3fee26
yagof <yagofer12@gmail.com>
2025-04-13 15:55:28 -0300

    validator

--- adapters/out/domain/request/agendamento_request.go
+++ adapters/out/domain/request/agendamento_request.go
@@ -1,1 +1,7 @@
 package request
+
+type AgendamentoRequest struct {
+	DataHoraEnvio string `json:"dataHoraEnvio" binding:"required"`
+	Destinatario  string `json:"destinatario" binding:"required"`
+	Mensagem      string `json:"mensagem" binding:"required,min=4,max=200"`
+}
