232c803de283a37d4be30f08ad17e6aacf6ef6ec
yagof <yagofer12@gmail.com>
2025-04-12 23:53:18 -0300

    mapper service

--- adapters/in/services/service.go
+++
@@ -1,6 +0,0 @@
-package services
-
-// mudar nome do arquivo
-// criado para usar mappers aqui ao invés de usar mappers no controller
-// agendamento_service.go já é a implementação de usecases
-// devemos injetar usecases nesse arquivo
