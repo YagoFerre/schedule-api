dde907259770bbfd4b802553d5679f5a8270d1e7
yagof <yagofer12@gmail.com>
2025-04-13 13:28:11 -0300

    mapper e request/response dtos

--- adapters/in/services/service_mapper.go
+++ adapters/in/services/service_mapper.go
@@ -1,5 +1,12 @@
 package mapper
 
+import (
+	"schedule-api/application/domain/model"
+	"schedule-api/configuration/rest_errors"
+)
+
 type AgendamentoMapper interface {
-	toModel()
+	HandleMapperAgendarEnvioNotificacao(agendamentoModel *model.AgendamentoModel) (*model.AgendamentoModel, *rest_errors.RestErr)
+	HandleMapperConsultarEnvioNotificacao(id int) (*model.AgendamentoModel, *rest_errors.RestErr)
+	HandleMapperCancelarEnvioNotificacao(agendamentoModel *model.AgendamentoModel) (*model.AgendamentoModel, *rest_errors.RestErr)
 }
